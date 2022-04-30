7784cc6e06199b772252bcdc5d39e44189276dcb
Milan Lustig <59667714+green726@users.noreply.github.com>
2022-04-30 18:27:39 -0400

    eeee

--- CompileEXE.cs
+++ CompileEXE.cs
@@ -2,8 +2,22 @@ using LLVMSharp;
 
 public static class EXE
 {
+    public static IntPtr targetTriple = LLVM.GetDefaultTargetTriple();
+    public static LLVMTargetRef target;
+    public static LLVMBool targetBool;
+    public static string errorMsg = "EEE";
+
     public static void compileEXE()
     {
+        LLVM.InitializeAllTargetInfos();
+        LLVM.InitializeAllTargets();
+        LLVM.InitializeAllTargetMCs();
+        LLVM.InitializeAllAsmParsers();
+        LLVM.InitializeAllAsmPrinters();
+
+        targetBool = LLVM.GetTargetFromTriple(targetTriple.ToString(), out target, out errorMsg);
+
+        Console.WriteLine("targetBool" + targetBool.Value);
 
 
     }
