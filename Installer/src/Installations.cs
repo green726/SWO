7727b48a86e5ed664087a14e2ef94a80c9ca64d0
Milan Lustig <59667714+green726@users.noreply.github.com>
2022-07-13 23:39:58 -0400

    Update Installations.cs

--- Installer/src/Installations.cs
+++ Installer/src/Installations.cs
@@ -1,20 +1,26 @@
 using System.Net;
+using System.IO.Compression;
 
 public static class Installations
 {
     public static WebClient client = new WebClient();
-    public static Uri HIPPath = new Uri("https://github.com/green726/HISS");
-    public static Uri languagePath = new Uri("https://github.com/green726/HISS");
+    public static Uri HIPUri = new Uri("https://github.com/green726/HISS/HIP");
+    public static Uri languageUri = new Uri("https://github.com/green726/HISS/Language");
 
 
     public static void installHIP(string path)
     {
-        client.DownloadFileAsync();
+        client.DownloadFileAsync(HIPUri, path + @"/HIP.zip");
+        //other HIP install stuff comes before the language install
+        ZipFile.ExtractToDirectory(path )
+
+
         installLanguage(path);
     }
 
     public static void installLanguage(string path)
     {
-        client.DownloadFileAsync();
+        client.DownloadFileAsync(languageUri, path + @"/Language.zip");
+
     }
 }
