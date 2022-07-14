8749b5fd1181dd22da935fd7133191fae05aa422
Milan Lustig <59667714+green726@users.noreply.github.com>
2022-07-13 23:10:32 -0400

    added the HIP folder

--- Installer/src/Installations.cs
+++ Installer/src/Installations.cs
@@ -1,14 +1,20 @@
-
+using System.Net;
 
 public static class Installations
 {
+    public static WebClient client = new WebClient();
+    public static Uri HIPPath = new Uri("https://github.com/green726/HISS");
+    public static Uri languagePath = new Uri("https://github.com/green726/HISS");
+
+
     public static void installHIP(string path)
     {
-        
+        client.DownloadFileAsync();
+        installLanguage(path);
     }
 
     public static void installLanguage(string path)
     {
-
+        client.DownloadFileAsync();
     }
 }
