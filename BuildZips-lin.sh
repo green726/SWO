a94d2f6cbe6eeed1669485553852a0090ce178fa
Milan Lustig <mguinangreen726@gmail.com>
2022-07-15 13:23:19 -0400

    linux build script added

---
+++ BuildZips-lin.sh
@@ -0,0 +1,8 @@
+zip -r ./HIP-win10-x64.zip ./HIP
+zip -r ./HIP-linux-x64.zip ./HIP
+cd ./Language
+dotnet publish -r win10-x64
+zip -r ../Language-win10-x64.zip ./bin/Debug/net6.0/win10-x64/publish/*
+dotnet publish -r linux-x64
+zip -r ../Language-linux-x64.zip ./bin/Debug/net6.0/linux-x64/publish/*
+cd ..
--- BuildZips-win.bat
+++ BuildZips-win.bat
@@ -2,7 +2,10 @@
 @REM dotnet publish -r win10-x64
 @REM zip -r ../HIP-win10-x64.zip ./bin
 tar -a -c -f ./HIP-win10-x64.zip ./HIP
+tar -a -c -f ./HIP-linux-x64.zip ./HIP
 cd ./Language
 dotnet publish -r win10-x64
 7z a -tzip ../Language-win10-x64.zip ./bin/Debug/net6.0/win10-x64/publish/*
+dotnet publish -r linux-x64
+7z a -tzip ../Language-linux-x64.zip ./bin/Debug/net6.0/linux-x64/publish/*
 cd ..
