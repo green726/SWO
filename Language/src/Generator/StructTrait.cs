cc3f832643a68d75123e09c84b8173f466e98f59
Milan Lustig <mguinangreen726@gmail.com>
2022-12-27 12:48:17 -0500

    created the trait generator

--- Language/src/Generator/StructTrait.cs
+++ Language/src/Generator/StructTrait.cs
@@ -13,5 +13,28 @@ public class StructTrait : Base
     public override void generate()
     {
         base.generate();
+
+        List<LLVMTypeRef> funcTypes = new List<LLVMTypeRef>();
+        foreach (AST.Prototype proto in trait.protos)
+        {
+            LLVMTypeRef[] funcArgTypes = new LLVMTypeRef[proto.arguments.Count];
+
+            int idx = 0;
+            foreach (KeyValuePair<string, AST.Type> arg in proto.arguments)
+            {
+                arg.Value.generator.generate();
+                funcArgTypes[idx] = gen.typeStack.Pop();
+                idx++;
+            }
+            LLVMTypeRef funcType = LLVM.FunctionType(((GeneratorTypeInformation)proto.returnType).getLLVMType(), funcArgTypes, false);
+            funcTypes.Add(funcType);
+        }
+
+        LLVMTypeRef structType = LLVM.StructCreateNamed(LLVM.GetGlobalContext(), trait.name);
+
+        LLVM.StructSetBody(structType, funcTypes.ToArray(), false);
+
+        gen.typeStack.Push(structType);
+        gen.namedTypesLLVM.Add(trait.name, structType);
     }
 }
